2dfc7aec0f9e5932c0f7797f3a1007d3b7dcc26c
Nevermind132 <ikrutalev@yandex.ru>
2021-08-24 19:46:10 +0400

    config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+from enum import Enum
+TOKEN = '1962903246:AAGOxJuJB7IZFG8fMJdK6sKQVM79bD5Ko98'
+chat_id = -1001541162816
+
+db_file='Data.vdb'
+
+class States(Enum):
+    S_START = "0"  
+    S_ENTER_URL="1" 
